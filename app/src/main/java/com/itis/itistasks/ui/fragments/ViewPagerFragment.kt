d16fd09a20c5f86da962131905f461a1bfb34231
Timur Medov <fyvakek@gmail.com>
2023-10-26 18:59:56 +0300

    2nd hw

--- app/src/main/java/com/itis/itistasks/ui/fragments/ViewPagerFragment.kt
+++ app/src/main/java/com/itis/itistasks/ui/fragments/ViewPagerFragment.kt
@@ -3,6 +3,7 @@ package com.itis.itistasks.ui.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.itis.itistasks.R
 import com.itis.itistasks.adapter.FragmentAdapter
 import com.itis.itistasks.databinding.FragmentViewPagerBinding
@@ -23,7 +24,41 @@ class ViewPagerFragment(private val itemCount: Int) :
             answers
         )
         binding = FragmentViewPagerBinding.bind(view)
-        binding?.vpFragment?.adapter = vpAdapter
+        binding?.run {
+            vpFragment.adapter = vpAdapter
+            vpFragment.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
+                var currPosition = 0
+                var currState = 0
+
+                override fun onPageScrolled(
+                    position: Int,
+                    positionOffset: Float,
+                    positionOffsetPixels: Int
+                ) {
+                    if (
+                        currState == ViewPager2.SCROLL_STATE_DRAGGING
+                        && positionOffsetPixels == 0
+                        ) {
+                        if (currPosition == 0) {
+                            vpFragment.currentItem = itemCount - 1
+                        } else if (currPosition == itemCount - 1) {
+                            vpFragment.currentItem = 0
+                        }
+                    }
+                    super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+                }
+
+                override fun onPageSelected(position: Int) {
+                    currPosition = position
+                    super.onPageSelected(position)
+                }
+
+                override fun onPageScrollStateChanged(state: Int) {
+                    currState = state
+                    super.onPageScrollStateChanged(state)
+                }
+            })
+        }
     }
 
     override fun onDestroyView() {
