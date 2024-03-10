185a7a541225af2d0612c6bf9995e7c2bf80befe
Timur Medov <91586824+Greenfuy@users.noreply.github.com>
2024-03-11 01:17:10 +0300

    remove open weather api

--- app/src/main/java/com/itis/itistasks/base/Keys.kt
+++ app/src/main/java/com/itis/itistasks/base/Keys.kt
@@ -3,7 +3,7 @@ package com.itis.itistasks.base
 object Keys {
     const val APP_ID_KEY = "appid"
     const val UNITS_KEY = "units"
-    const val OPEN_WEATHER_API_KEY = "e12dd6d9d71c445e8b6a0309c22c13a4"
+    const val OPEN_WEATHER_API_KEY = "API_KEY"
     const val OPEN_WEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5/"
     const val OPEN_WEATHER_ICON_URL = "https://openweathermap.org/img/wn/"
 }
